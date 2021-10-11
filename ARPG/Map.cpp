7f48b7cfb5bc2af77eb6139b5909ca588d042534
Mathieu Larue <mat_1794@hotmail.com>
2021-10-11 00:11:38 -0400

    Map

--- ARPG/Map.cpp
+++ ARPG/Map.cpp
@@ -4,6 +4,46 @@
 
 using namespace nlohmann;
 
+DrawingInfo createDrawingInfo(json sprite, SDL_Surface* surface, int x, int y)
+{
+	DrawingInfo drawingInfo;
+	SDL_Rect src = SDL_Rect();
+	SDL_Rect dst = SDL_Rect();
+
+	int width;
+	int height;
+	width = height = sprite["size"];
+
+	src.x = sprite["originX"];
+	src.y = sprite["originY"];
+	src.w = width;
+	src.h = height;
+
+	dst.x = x;
+	dst.y = y;
+	dst.w = width;
+	dst.h = height;
+
+	drawingInfo.srcRect = src;
+	drawingInfo.dstRect = dst;
+	drawingInfo.surface = surface;
+
+	return drawingInfo;
+}
+
+json findSprite(json j, string id)
+{
+	for (auto& sprite : j["sprites"])
+	{
+		if (sprite["id"] == id)
+		{
+			return sprite;
+		}
+	}
+
+	throw;
+}
+
 void Map::init(string surfaceName, string fileName, SurfaceLoader* surfaceLoader, SDL_Surface* windowSurface)
 {
 	surface = surfaceLoader->loadSurface(surfaceName, windowSurface);
@@ -14,31 +54,26 @@ void Map::init(string surfaceName, string fileName, SurfaceLoader* surfaceLoader
 	json j;
 	i >> j;
 
-	for (auto& placement : j["map"]) {
-		DrawingInfo drawingInfo;
+	auto& layer1 = j["layer1"];
+	json layer1Sprite = findSprite(j, layer1["id"]);
 
-		SDL_Rect src = SDL_Rect();
-		for (auto& sprite : j["sprites"])
+	int width;
+	int height;
+	width = height = layer1Sprite["size"];
+
+	for (int i = 0; i < 160 / width; ++i)
+	{
+		for (int j = 0; j < 160 / height; ++j)
 		{
-			if (sprite["id"] == placement["id"])
-			{
-				src.x = sprite["originX"];
-				src.y = sprite["originY"];
-				src.w = 16;
-				src.h = 16;
-				break;
-			}
+			DrawingInfo drawingInfo = createDrawingInfo(layer1Sprite, surface, i * width, j * height);
+			drawingInfos.push_back(drawingInfo);
 		}
+	}
 
-		SDL_Rect dst = SDL_Rect();
-		dst.x = placement["x"];
-		dst.y = placement["y"];
-		dst.w = 16;
-		dst.h = 16;
-
-		drawingInfo.srcRect = src;
-		drawingInfo.dstRect = dst;
-		drawingInfo.surface = surface;
+	for (auto& layer2 : j["layer2"])
+	{
+		json layer2Sprite = findSprite(j, layer2["id"]);
+		DrawingInfo drawingInfo = createDrawingInfo(layer2Sprite, surface, layer2["x"], layer2["y"]);
 		drawingInfos.push_back(drawingInfo);
 	}
 }
