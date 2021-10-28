a85f3b2fd8d129bee43af0577149a2aaf8812a6e
Mathieu Larue <mat_1794@hotmail.com>
2021-10-27 22:14:30 -0400

    Enemy move

--- ARPG/Enemy.cpp
+++ ARPG/Enemy.cpp
@@ -1,10 +1,25 @@
 #include "Enemy.h"
+#include <random>
 
 const int animationThreshold = 20;
+const int moveThreshold = 40;
 Animation mainAnimation = Animation();
+
+std::mt19937 seed;
+std::uniform_int_distribution<std::mt19937::result_type> moveRng;
+std::uniform_int_distribution<std::mt19937::result_type> stepRng;
 
 void Enemy::init(SDL_Surface* windowSurface, SurfaceLoader* surfaceLoader, AnimationLoader* animationLoader, int screenWidth, int screenHeight)
 {
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	seed = rng;
+	
+	std::uniform_int_distribution<std::mt19937::result_type> moveFunction(0, 2);
+	std::uniform_int_distribution<std::mt19937::result_type> stepFunction(0, 4);
+	moveRng = moveFunction;
+	stepRng = stepFunction;
+
 	animationLoader->loadAnimation(&mainAnimation, "tentacle.json", "walking", surfaceLoader->loadSurface("tentacle.png", windowSurface));
 
 	setAnimation(&mainAnimation, true);
@@ -26,6 +41,30 @@ std::vector<DrawingInfo> Enemy::tick(const Uint8* keyState, int totalFrame, std:
 	if (shouldAnimate)
 	{
 		incrementFrame();
+	}
+
+	const bool shouldMove = totalFrame % moveThreshold == 0;
+	if (shouldMove)
+	{
+		int randomX = moveRng(seed);
+		int randomY = moveRng(seed);
+		int step = stepRng(seed);
+
+		if (randomX == 0) {
+			x += -step;
+		}
+		else if (randomX == 1)
+		{
+			x += step;
+		}
+
+		if (randomY == 0) {
+			y += -step;
+		}
+		else if (randomY == 1)
+		{
+			y += step;
+		}
 	}
 
 	std::vector<DrawingInfo> drawingInfos;
